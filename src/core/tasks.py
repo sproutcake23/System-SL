bcb3260a0e1f3793b79c6501eb7cb80a0bc5c7a6
Gishnu Gompa <75305540+sproutcake23@users.noreply.github.com>
2026-02-15 21:56:57 +0530

    bugfix: fix github tracking data/ even though it is mentioned in gitignore
    - modify `get_tasks_file_path` function to create data/ if it does not
    exist
    - rename the existing data/ to demo-data/

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -7,8 +7,12 @@ from datetime import datetime
 def get_tasks_file_path(filename):
     curr_dir = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.dirname(os.path.dirname(curr_dir))
+    data_path = os.path.join(project_root, "data")
 
-    return os.path.join(project_root, "data", filename)
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+
+    return os.path.join(data_path, filename)
 
 
 TASKS_FILE_PATH = get_tasks_file_path("tasks.json")
